7738b99d3fe1410007b8482589025fef03e0fef6
Marcin Mider <marcin.mider@gmail.com>
2024-02-24 11:56:24 +0100

    moving back temporal_data

--- src/IMUDevNNLib.jl
+++ src/IMUDevNNLib.jl
@@ -6,6 +6,10 @@ const Abstract3Tensor = AbstractArray{<:Any,3}
 
 include("utils.jl")
 include("batched_operations.jl")
+include("temporal_data.jl")
+
+# temporal_data.jl
+export TemporalData, temporal_data, num_samples, num_timepoints, state_dim, obs_dim
 
 # utils.jl
 export skipfirstobs, eachslicelastdim, selectfirstobs
